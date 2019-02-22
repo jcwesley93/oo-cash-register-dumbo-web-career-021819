c782ba3116f8fed522d2a2f00ec586758ee5c4e4
Jaela Wesley <jcwesley93@gmail.com>
2019-02-22 18:57:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
+
+attr_accessor :discount, :total
+
+def initialize(discount = 0)
+  @total = 0 
+  @discount = discount 
+end 
+
+def add_item(title, price, quantity = 0)
+  if quantity == 0 
+    @total += price
+  elsif quantity > 0 
+    @total += (price * quantity)
+  end
+end
+  
+  
+  
+end
+
+
 
